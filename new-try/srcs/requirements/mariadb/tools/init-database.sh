0daba90f7872f5ff0f28b4905417c1b3cdb4fadb
gamoreno <gamoreno@gamoreno.gamoreno>
2024-01-06 19:54:27 -0500

    good mariadb

--- new-try/srcs/requirements/mariadb/tools/init-database.sh
+++ new-try/srcs/requirements/mariadb/tools/init-database.sh
@@ -1,10 +1,8 @@
-!/bin/bash
+#!/bin/bash
 
-#   Check if the wordpress database is already created
 echo "=> Checking if \`$DB_NAME\` database exists . . ."
 if [ ! -d "/var/lib/mysql/$DB_NAME" ]; then
 
-    #   Check if MariaDB is already running
     if ! service mariadb status > /dev/null; then
         echo "=> MariaDB service is not running, starting it . . ."
         service mariadb start
@@ -13,31 +11,28 @@ if [ ! -d "/var/lib/mysql/$DB_NAME" ]; then
         echo "=> MariaDB service is already running."
     fi
 
-    echo "=> creating \`$DB_NAME\` database . . ."
-    # CREATE WORDPRESS DATABASE
-    mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+    echo "MariaDB server started. Setting up user: ${DB_USER}"
+    run_cmd="mysql -u root -p${GNRL_ROOT_PASSWORD} -e"
 
-    # CREATE USER
-    echo "=> creating user . . ."
-    mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PASSWORD';"
+    echo "Create db if it doesn't exist"
+    $run_cmd "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
 
-    # PRIVILEGES USER FOR ALL IP ADRESSES
-    mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_USER_PASSWORD';"
+    echo "Create user if it doesn't exist"
+    $run_cmd "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_USER_PASSWORD}';"
 
-    mysql -e "FLUSH PRIVILEGES;"
+    echo "Grant all privileges on ${DB_NAME} to ${DB_USER}"
+    $run_cmd "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'%';"
 
-    # FORCE AUTHENTIFICATION WITH PASSWORD FOR ROOT
-    mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$GNRL_ROOT_PASSWORD';"
+    echo "Flush privileges"
+    $run_cmd "FLUSH PRIVILEGES;"
 
     mysqladmin -u root -p${GNRL_ROOT_PASSWORD} shutdown
 
     echo "=> MariaDB database and user were created successfully! "
 
-    # Shutdown MariaDB gracefully if it was started in this script
     if [ -f /var/run/mysqld/mysqld.pid ]; then
         echo "=> Shutting down MariaDB..."
         service mariadb stop
-        # kill $(cat /var/run/mysqld/mysqld.pid)
         sleep 2
     fi
 
@@ -46,4 +41,3 @@ else
 fi
 
 exec mysqld
-
